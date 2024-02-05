9b94f15c8f0926677f91a8f6047558cd6b614412
Lameckwh <lameckmbewe6@gmail.com>
2024-02-06 00:19:11 +0200

    added forms

--- playground/forms.py
+++ playground/forms.py
@@ -1,8 +1,9 @@
 from django.forms import ModelForm
 from .models import Room, Topic
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields  = '__all__'
-        
+        fields = "__all__"
+        exclude = ["host", "participants"]
--- playground/views.py
+++ playground/views.py
@@ -104,21 +104,40 @@ def room(request, pk):
     return render(request, "playground/room.html", context)
 
 
-def userProfile(request):
-    context = {}
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    topics = Topic.objects.all()
+    room_messages = user.messages_set.all()
+    context = {
+        "user": user,
+        "rooms": rooms,
+        "room_messages": room_messages,
+        "topics": topics,
+    }
     return render(request, "playground/profile.html", context)
 
 
 @login_required(login_url="login")
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {"form": form}
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
+        )
+        return redirect("home")
+
+    context = {
+        "form": form,
+        "topics": topics,
+    }
     return render(request, "playground/room_form.html", context)
 
 
@@ -126,6 +145,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("Your are not allowed to edit this room")
@@ -135,7 +155,10 @@ def updateRoom(request, pk):
         if form.is_valid():
             form.save()
             return redirect("home")
-    context = {"form": form}
+    context = {
+        "form": form,
+        "topics": topics,
+    }
     return render(request, "playground/room_form.html", context)
 
 
